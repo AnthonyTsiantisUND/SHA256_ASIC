1c5602a44e2c871408ae0c2232b1587ae1a67797
Anthony Tsiantis <atsianti@student10.cse.nd.edu>
2024-12-14 01:19:52 -0500

    Completed Circuit logic (needs major debug)

--- sha256.v
+++ sha256.v
@@ -5,14 +5,14 @@
 	input_complete = input complete
 	input_data = 8 bits of input
 	hased_data = 16 bits of hashed output
+	NOTE: Data is inputted left to right and the hash is outputted left to right
 */
-
-
 module SHA256 #() (
 	input logic clk, reset, load_enable, input_complete,
 	input logic[7:0] input_data,
 	output logic[15:0] hashed_data
 );
+	reg output_ready = 0;
 	// PREPROCESSING THE DATA
 	// STEP 1: Get input 1 bytes/8 bits/one character at a time
 	reg [511:0] data_storage; // 512 bit input (We are only inputting 504 bits to allow for the additional 1 + padding
@@ -55,38 +55,73 @@ module SHA256 #() (
 	data_storage [7:0] = length;
 
 	
-	// HASHING THE DATA
-	// STEP 5: Inital Hash constants
-	// 		   SHA256 uses the first 32 bits of the fractional part of 
-	//		   the square roots of the first 64 primes.
-	//		   To start, I have initalized the hash values to the fractional
-	//		   part of the first 8 primes.
-	//		   We can then use the function SHA256_Constants to cycle through the rest
-
-	reg [255:0] hash_values = {
-		32'h6A09E667, 32'hBB67AE85, 32'h3C6EF372, 32'hA54FF53A,
-		32'h510E527F, 32'h9B05688C, 32'h1F83D9AB, 32'h5BE0CD19
-	};
-
-
 
 	// STEP 6: Append 3 more 512 blocks of 0
-	reg data_storage2 [2047:0] = {data_storage, 1536'b0}
+	reg weight_matrix [2047:0] = {data_storage, 1536'b0}
 
 	// STEP 7: Preform the first scramble
-	SHA256_Scramble scramble_inst (
+	Weights weights (
 		.clock(clock),
-		.data(data_storage2),
-		.out(data_storage2)
+		.data(weight_matrix),
+		.out(weight_matrix)
 	);
 
 
 	// STEP 8: Compression Cycle
 	// 		   Initalize the 8 hash values to our presets
 	//         with our 64 word data, we will modify these hash values over and over
+	reg [255:0] hash_values = {
+		32'h6A09E667, 32'hBB67AE85, 32'h3C6EF372, 32'hA54FF53A,
+		32'h510E527F, 32'h9B05688C, 32'h1F83D9AB, 32'h5BE0CD19
+	};
+	reg [31:0] a = hash_values[255:224];
+	reg [31:0] b = hash_values[223:192];
+	reg [31:0] c = hash_values[191:160];
+	reg [31:0] d = hash_values[159:128];
+	reg [31:0] e = hash_values[127:96];
+	reg [31:0] f = hash_values[95:64];
+	reg [31:0] g = hash_values[63:32]; 
+	reg [31:0] h = hash_values[31:0];
+	reg [31:0] constant_i, weight_i;
+	SHA_256_Constants (
+		.clock(clock), .reset(reset), .output_constant(constant_i)
+	);
 
+	always @(posedge clock) begin
+		for (integer i = 0; i < 64; i = i + 1) begin
+			assign weight_i = weight_matrix[(i*32)+31:i*32];
+			Compression compress (
+				.constant_i(constant_i), .weight_i(weight_i),
+				.a_in(a), .b_in(b), .c_in(c), .d_in(d), 
+				.e_in(e), .f_in(f), .g_in(g), .h_in(h),
+				.a_out(a), .b_out(b), .c_out(c), .d_out(d), 
+				.e_out(e), .f_out(f), .g_out(g), .h_out(h)
+			);
+		end
+	end
 
-
+	// STEP 9: Add the original hashes back to our new values
+	a <= a + hash_values[255:224];
+	b <= b + hash_values[223:192];
+	c <= c + hash_values[191:160];
+	d <= b + hash_values[159:128];
+	e <= e + hash_values[127:96];
+	f <= f + hash_values[95:64];
+	g <= g + hash_values[63:32]; 
+	h <= h + hash_values[31:0];
+	
+	
+	// STEP 10: Send the data out, 16 bits at a time
+	counter <= 0;
+	reg data_out[255:0] = {a, b, c, d, e, f, g, h};
+	reg output_ready = 1;
+	always @(posedge clock) begin
+		if (output_ready && counter < 32) begin
+			hashed_data <= data_out[((32-i)*16)-1:(32-(i+1))*16];
+			counter <= counter + 1;
+		end	
+	
+	end
 
 
 
@@ -95,7 +130,82 @@ module SHA256 #() (
 		data_storage = 512'b0;
 		counter = 0;
 		input_complete = 0;
+		output_ready = 0;
 	end
+
+endmodule
+
+// This is the first right rotate module
+// Our goal is to take a 32 bit (word) input from our 2048 bit dataset
+// and for each word from [16-63] we will proform the following function
+// 	word1 = (word1[i-15] rightrotate 7) xor (word1[i-15] rightrotate 18) xor (word1[i-15] righthift 3)
+//	word2 = (word2[i-2] rightrotate 17) xor (word2[i-2] rightrotate 19) xor (word2[i-2] righthift 10)
+// 	new_word = data_word[i-16] + word0 + data_word[i-7] + word1
+
+module Weights (
+	input wire clock,
+	input wire [2047:0] data,
+	output wire [2047:0] out
+);
+	always @(posedge clock) begin
+		for (integer i = 16; i < 64; i = i + 1) begin
+			reg [31:0] word0 = data[((i-15)*32)+31:((i-15)*32)]; // Get word0 from data
+			assign word0 = ({word0[6:0], word0[31:7]} ^ {word0[17:0], word0[31:18]} ^ (word0 >> 3));
+
+			reg [31:0] word1 = data[((i-2)*32)+31:((i-2)*32)]; // Get word1 from data
+			assign word1 = ({word1[16:0], word1[31:17]} ^ {word1[18:0], word1[31:19]} ^ (word1 >> 10));
+
+			assign data[(i*32)+31:i*32] = data[((i-16)*32)+31:((i-16)*32)] + word0 + data[((i-7)*32)+31:((i-7)*32)] + word1;
+		end
+	end
+	assign out = data;
+endmodule
+
+
+// This is our compression / hashing function
+//	The first input is the constant and weight words at out index i
+//	The second input 8, 32 bit words that we will continuously hash
+//	The output is the next input to this function (Originally, all words are our 
+// 		first 8 prime constants
+//	Computations:
+//			hash1 = (e rightrotate 6) xor (e rightrotate 11) xor (e rightrotate 25)
+//			hash0 = (a rightrotate 2) xor (a rightrotate 13) xor (a rightrotate 22)
+//			choose = (e and f) xor ((!e) and g)
+//			majority = (a and b) xor (a and c) xor (b and c)
+//			word0 = h + hash1 + choose + constant + weight
+//			word1 = hash0 + choose + constant + weight
+//	Outputs:
+//		a <= word0 + word1
+//		b <= a
+//		c <= b
+//		d <= c
+//		e <= d + word0
+//		f <= e
+//		g <= f
+//		h <= g
+module Compression (
+	input [31:0] constant_i, weight_i,
+	input [31:0] a_in, b_in, c_in, d_in, e_in, f_in, g_in, h_in,
+	output [31:0] a_out, b_out, c_out, d_out, e_out, f_out, g_out, h_out
+);
+	wire [31:0] hash1 = ({e_in[5:0], e_in[31:6]} ^ {e_in[10:0], e_in[31:11]} ^ {e_in[24:0], e_in[31:25]});
+	wire [31:0] hash0 = ({a_in[1:0], a_in[31:2]} ^ {a_in[12:0], a_in[31:13]} ^ {a_in[21:0], a_in[31:22]});
+	wire [31:0] choose_e_f_g = ((e_in & f_in) ^ ((~e_in) & g_in));
+	wire [31:0] majority_a_b_c = (a_in & b_in) ^ (a_in & c_in) ^ (b_in & c_in);
+	
+	// Compute values
+	wire [31:0] word0 = h_in + hash1 + choose_e_f_g + constant_i + weight_i;
+	wire [31:0] word1 = hash0 + majority_a_b_c;
+	
+	// Assign outputs
+	assign a_out = word0 + word1;
+	assign b_out = a_in;
+	assign c_out = b_in;
+	assign d_out = c_in;
+	assign e_out = d_in + word0;
+	assign f_out = e_in;
+	assign g_out = f_in;
+	assign h_out = g_in;
 
 endmodule
 
@@ -142,31 +252,4 @@ module SHA256_Constants (
 			constant_array = rotated_array;
 		end
 	end
-endmodule
-
-
-// This is the first right rotate module
-// Our goal is to take a 32 bit (word) input from our 2048 bit dataset
-// and for each word from [16-63] we will proform the following function
-// 	word1 = (word1[i-15] rightrotate 7) xor (word1[i-15] rightrotate 18) xor (word1[i-15] righthift 3)
-//	word2 = (word2[i-2] rightrotate 17) xor (word2[i-2] rightrotate 19) xor (word2[i-2] righthift 10)
-// 	new_word = data_word[i-16] + word0 + data_word[i-7] + word1
-
-module SHA256_Scramble (
-	input wire clock,
-	input wire [2047:0] data,
-	output wire [2047:0] out
-);
-	always @(posedge clock) begin
-		for (integer i = 16; i < 64; i = i + 1) begin
-			reg [31:0] word0 = data[((i-15)*32)+31:((i-15)*32)]; // Get word0 from data
-			assign word0 = ({word0[6:0], word0[31:7]} ^ {word0[17:0], word0[31:18]} ^ (word0 >> 3));
-
-			reg [31:0] word1 = data[((i-2)*32)+31:((i-2)*32)]; // Get word1 from data
-			assign word1 = ({word1[16:0], word1[31:17]} ^ {word1[18:0], word1[31:19]} ^ (word1 >> 10));
-
-			assign data[(i*32)+31:i*32] = data[((i-16)*32)+31:((i-16)*32)] + word0 + data[((i-7)*32)+31:((i-7)*32)] + word1;
-		end
-	end
-	assign out = data;
 endmodule
